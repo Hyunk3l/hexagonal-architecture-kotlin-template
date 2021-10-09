74a7c74336c9e42ba92db93a79343513d588319d
Fabrizio Di Napoli <hyunk3l@gmail.com>
2021-10-09 18:02:47 +0200

    Create sh to run tests both locally and in CI

---
+++ runTests.sh
@@ -0,0 +1,6 @@
+
+copier . ../hexagonal-architecture-kotlin-template-test && \
+  cd ../hexagonal-architecture-kotlin-template-test && \
+  ./gradlew clean build && \
+  cd ../ && \
+  rm -r hexagonal-architecture-kotlin-template-test/
