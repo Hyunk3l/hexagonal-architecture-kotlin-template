4c6c1f8f44c77eac957f0f1c82ce048d750a82e1
Fabrizio Di Napoli <hyunk3l@gmail.com>
2021-10-16 23:46:43 +0200

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.5.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("io.gitlab.arturbosch.detekt") version "1.18.1"
 	kotlin("jvm") version "1.5.31"
 	kotlin("plugin.spring") version "1.5.31"
 }
