be0ce7dca182fe1cd2453ca4a32014b810757cad
Fabrizio Di Napoli <hyunk3l@gmail.com>
2021-12-01 23:11:51 +0100

    Kotlin to 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 	id("org.springframework.boot") version "2.5.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	id("io.gitlab.arturbosch.detekt") version "1.18.1"
-	kotlin("jvm") version "1.5.31"
-	kotlin("plugin.spring") version "1.5.31"
+	kotlin("jvm") version "1.6.0"
+	kotlin("plugin.spring") version "1.6.0"
 }
 
 group = "[[ group_name ]]"
