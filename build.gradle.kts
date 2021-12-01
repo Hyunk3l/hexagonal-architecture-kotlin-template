ba95ab210d7cb88441c18714ac612faba9dba2b3
Fabrizio Di Napoli <hyunk3l@gmail.com>
2021-12-01 23:08:08 +0100

    Upgrade some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,18 +17,18 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.springframework.boot:spring-boot-starter-webflux:2.5.6")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("io.arrow-kt:arrow-core:1.0.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.5.2-native-mt")
+	implementation("io.arrow-kt:arrow-core:1.0.1")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6") {
 		exclude(module = "mockito-core")
 	}
-	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("io.projectreactor:reactor-test:3.4.12")
 	testImplementation("org.testcontainers:testcontainers:1.16.2")
 	testImplementation("org.testcontainers:junit-jupiter:1.16.2")
 	testImplementation("org.testcontainers:postgresql:1.16.2")
