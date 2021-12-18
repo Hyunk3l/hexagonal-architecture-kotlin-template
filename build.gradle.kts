7d43ca4c02c899cb2176222bb1acef52b642d723
Fabrizio Di Napoli <hyunk3l@gmail.com>
2021-12-18 11:09:42 +0100

    Upgrade kotlin to 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("io.gitlab.arturbosch.detekt") version "1.18.1"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
 }
 
 group = "[[ group_name ]]"
