2651f70b786b8781a80040bb32632c1feb542c64
Fabrizio Di Napoli <hyunk3l@gmail.com>
2022-04-25 23:21:30 +0200

    Upgrade dependencies in bulk

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.6"
+    id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("io.gitlab.arturbosch.detekt") version "1.20.0-RC2"
     kotlin("jvm") version "1.6.20"
@@ -21,13 +21,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.6")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.21")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1-native-mt")
-    implementation("io.arrow-kt:arrow-core:1.0.1")
+    implementation("io.arrow-kt:arrow-core:1.1.2")
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.postgresql:postgresql:42.3.3")
-    implementation("org.flywaydb:flyway-core:8.5.7")
+    implementation("org.postgresql:postgresql:42.3.4")
+    implementation("org.flywaydb:flyway-core:8.5.9")
     implementation(platform("org.apache.logging.log4j:log4j-bom:2.17.0"))
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
@@ -35,13 +35,13 @@ dependencies {
         exclude(group = "junit", module = "junit")
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("io.projectreactor:reactor-test:3.4.16")
-    testImplementation("org.testcontainers:testcontainers:1.16.3")
-    testImplementation("org.testcontainers:junit-jupiter:1.16.3")
-    testImplementation("org.testcontainers:postgresql:1.16.3")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.2.2")
-    testImplementation("io.kotest:kotest-assertions-json:5.2.2")
-    testImplementation("io.kotest:kotest-runner-junit5:5.2.2")
+    testImplementation("io.projectreactor:reactor-test:3.4.17")
+    testImplementation("org.testcontainers:testcontainers:1.17.1")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.1")
+    testImplementation("org.testcontainers:postgresql:1.17.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.2.3")
+    testImplementation("io.kotest:kotest-assertions-json:5.2.3")
+    testImplementation("io.kotest:kotest-runner-junit5:5.2.3")
     testImplementation("io.rest-assured:rest-assured:5.0.1")
     testImplementation("io.rest-assured:json-path:5.0.1")
     testImplementation("io.rest-assured:xml-path:5.0.1")
