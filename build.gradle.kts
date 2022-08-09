202bf581a1336c274cc9799708e3e6cc785c9313
Fabrizio Di Napoli <hyunk3l@gmail.com>
2022-08-09 20:29:39 +0200

    Minor and bugfixes dependencies upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.1"
+    id("org.springframework.boot") version "2.7.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("io.gitlab.arturbosch.detekt") version "1.20.0"
+    id("io.gitlab.arturbosch.detekt") version "1.21.0"
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.7.10"
 }
@@ -20,33 +20,33 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.6")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.2")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.7")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
     implementation("io.arrow-kt:arrow-core:1.1.2")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.postgresql:postgresql:42.3.6")
-    implementation("org.flywaydb:flyway-core:8.5.12")
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.17.0"))
+    implementation("org.flywaydb:flyway-core:9.0.4")
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.18.0"))
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(module = "mockito-core")
         exclude(group = "junit", module = "junit")
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("io.projectreactor:reactor-test:3.4.18")
-    testImplementation("org.testcontainers:testcontainers:1.17.2")
-    testImplementation("org.testcontainers:junit-jupiter:1.17.2")
-    testImplementation("org.testcontainers:postgresql:1.17.2")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.3.1")
-    testImplementation("io.kotest:kotest-assertions-json:5.3.1")
-    testImplementation("io.kotest:kotest-runner-junit5:5.3.1")
+    testImplementation("io.projectreactor:reactor-test:3.4.21")
+    testImplementation("org.testcontainers:testcontainers:1.17.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.3")
+    testImplementation("org.testcontainers:postgresql:1.17.3")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.4.1")
+    testImplementation("io.kotest:kotest-assertions-json:5.4.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.1")
     testImplementation("io.rest-assured:rest-assured:5.1.1")
     testImplementation("io.rest-assured:json-path:5.1.1")
     testImplementation("io.rest-assured:xml-path:5.1.1")
     testImplementation("io.rest-assured:json-schema-validator:5.1.1")
-    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("io.mockk:mockk:1.12.5")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
     testImplementation("com.tngtech.archunit:archunit:1.0.0-rc1")
 }
