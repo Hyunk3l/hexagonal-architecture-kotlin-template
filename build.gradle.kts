9c3dff1325eed13008d6c3fcd52711dc89bbc544
Fabrizio Di Napoli <hyunk3l@gmail.com>
2022-11-05 20:54:13 +0100

    Minor upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.4"
+    id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
     id("io.gitlab.arturbosch.detekt") version "1.21.0"
     kotlin("jvm") version "1.7.20"
@@ -27,7 +27,7 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:1.1.2")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.postgresql:postgresql:42.5.0")
-    implementation("org.flywaydb:flyway-core:9.4.0")
+    implementation("org.flywaydb:flyway-core:9.6.0")
     implementation(platform("org.apache.logging.log4j:log4j-bom:2.19.0"))
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
@@ -35,13 +35,13 @@ dependencies {
         exclude(group = "junit", module = "junit")
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("io.projectreactor:reactor-test:3.4.23")
+    testImplementation("io.projectreactor:reactor-test:3.4.24")
     testImplementation("org.testcontainers:testcontainers:1.17.4")
     testImplementation("org.testcontainers:junit-jupiter:1.17.4")
     testImplementation("org.testcontainers:postgresql:1.17.4")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.0")
-    testImplementation("io.kotest:kotest-assertions-json:5.5.0")
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.3")
+    testImplementation("io.kotest:kotest-assertions-json:5.5.3")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.3")
     testImplementation("io.rest-assured:rest-assured:5.2.0")
     testImplementation("io.rest-assured:json-path:5.2.0")
     testImplementation("io.rest-assured:xml-path:5.2.0")
