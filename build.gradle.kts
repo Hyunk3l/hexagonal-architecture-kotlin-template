1c4e6a195aa89c7b819b22c880acfb420485b99f
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-07-02 09:47:04 +0200

    Upgrade kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.springframework.boot") version "3.1.1"
     id("io.spring.dependency-management") version "1.1.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.0"
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
 }
 
 group = "{{ group_name }}"
