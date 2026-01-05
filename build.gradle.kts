790dbe3044168f28006678819930df66029e3948
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2026-01-05 20:30:47 +0100

    Restore

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
         exclude(group = "junit", module = "junit")
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
     testImplementation("io.projectreactor:reactor-test:3.8.1")
     testImplementation("org.testcontainers:testcontainers:2.0.3")
     testImplementation("org.testcontainers:junit-jupiter:1.21.4")
